90ab4eee2b5792daa4160b06e62b3e06a7d7e5ec
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-09-12 20:52:23 -0300

    Segunda função indexOf

--- FunçõesArray.js
+++ FunçõesArray.js
@@ -1,11 +1,21 @@
 // Função Concat
 // Serve para juntar dois ou mais arrays.
-// Exemplos
+// Exemplo
+
+// let classeA = ["João", "Maria", "Zé", "Felipe", "Marcos"]
+// let classeB = ["Leticia", "Francisco", "Fernando"]
+// let classeC = ["Moisés", "Helena"]
+
+// let resultado = classeA.concat(classeB, classeC)
+
+// console.log(resultado)
+
+// Função IndexOf
+// Retorna a posição daquele valor que escolhemos.
+// Exemplo
 
 let classeA = ["João", "Maria", "Zé", "Felipe", "Marcos"]
-let classeB = ["Leticia", "Francisco", "Fernando"]
-let classeC = ["Moisés", "Helena"]
 
-let resultado = classeA.concat(classeB, classeC)
+let resultado = classeA.indexOf("Marcos")
 
 console.log(resultado)
