7e9a221f09a0c7454ed4d0dd536c840fe47db852
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-09-12 20:29:01 -0300

    Primeira função concat

---
+++ FunçõesArray.js
@@ -0,0 +1,11 @@
+// Função Concat
+// Serve para juntar dois ou mais arrays.
+// Exemplos
+
+let classeA = ["João", "Maria", "Zé", "Felipe", "Marcos"]
+let classeB = ["Leticia", "Francisco", "Fernando"]
+let classeC = ["Moisés", "Helena"]
+
+let resultado = classeA.concat(classeB, classeC)
+
+console.log(resultado)
