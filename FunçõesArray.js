14eb9d132401094743e3b8ffbf623a65fc893487
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-09-12 21:30:49 -0300

    Terceira função pop e Quarta função Splice

--- FunçõesArray.js
+++ FunçõesArray.js
@@ -14,8 +14,49 @@
 // Retorna a posição daquele valor que escolhemos.
 // Exemplo
 
-let classeA = ["João", "Maria", "Zé", "Felipe", "Marcos"]
+// let classeA = ["João", "Maria", "Zé", "Felipe", "Marcos"]
 
-let resultado = classeA.indexOf("Marcos")
+// let resultado = classeA.indexOf("Marcos")
 
-console.log(resultado)
+// console.log(resultado)
+
+// Função Pop
+// Serve para excluir o último elemento do array
+// Exemplo
+
+// let classeA = ["João", "Maria", "Zé", "Felipe", "Marcos"]
+
+// let resultado = classeA.pop()
+
+// console.log(resultado)
+// console.log(classeA)
+
+// Função Splice
+// Serve para adcionar e/ou remover elementos do array. No primeiro parâmetro, indicamos a posição do array, e no segundo
+// a quantidade de elementos que deseja excluir a partir da posição indicada.
+// Exemplo
+
+// let notas = [10, 5, 1, 2, 8]
+// console.log(notas)
+
+// notas.splice(1,3)
+// console.log(notas)
+
+// também conseguimos somente adicionar elementos no array, para isso no segundo parâmetro deixamos o 0(zero), e depois
+// adiocamos os elementos novos, por exemplo.
+
+// let notas2 = [10, 5, 1, 2, 8]
+// console.log(notas2)
+
+// notas2.splice(1,0, 7, 8, 15)
+// console.log(notas2)
+
+// também podemos remover e adicionar elementos ao mesmo tempo, por exemplo:
+
+// let notas3 = [10, 5, 1, 2, 8]
+// console.log(notas3)
+
+// notas3.splice(3, 2, 7, 8, 15)
+// console.log(notas3)
+
+
